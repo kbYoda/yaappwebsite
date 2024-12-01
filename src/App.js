1ff7d9f5e8dbfefb0eca6e41362c52929a4c1aef
Kriti Bhayani <148117368+kbYoda@users.noreply.github.com>
2024-11-30 23:51:23 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Features from './pages/Features';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import SignIn from './pages/SignIn';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
